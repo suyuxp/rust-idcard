5b2711cc70475a6f0370179f61e91a19b2e68f5b
Su Yu <suyu@intermaker.cn>
2019-10-13 00:27:44 +0800

    增加阿里云身份证+姓名校验

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+use reqwest::header::AUTHORIZATION;
+use reqwest::Error;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct CodeNameResp {
+    name: String,
+    #[serde(rename = "idNo")]
+    id_no: String,
+    #[serde(rename = "respMessage")]
+    resp_message: String,
+    #[serde(rename = "respCode")]
+    resp_code: String,
+    province: Option<String>,
+    city: Option<String>,
+    county: Option<String>,
+    birthday: Option<String>,
+    sex: Option<String>,
+    age: Option<String>,
+}
+
+pub fn validate_codename(idcard: &str, name: &str, appcode: &str) -> Result<CodeNameResp, Error> {
+    let url = "https://idenauthen.market.alicloudapi.com/idenAuthentication";
+    let params = [("idNo", idcard), ("name", name)];
+    let client = reqwest::Client::new();
+    let res = client
+        .post(url)
+        .form(&params)
+        .header(AUTHORIZATION, format!("APPCODE {}", appcode))
+        .send()?
+        .json::<CodeNameResp>()?;
+
+    Ok(res)
+}
+
 pub fn validate(idcard: &str) -> bool {
     let weights = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
     let sum: u32 = idcard
@@ -39,5 +75,26 @@ mod tests {
         assert_eq!(super::validate("150404198402"), false);
         assert_eq!(super::validate("1"), false);
         assert_eq!(super::validate(""), false);
+
+        assert_eq!(
+            super::validate_codename(
+                "510108197205052137",
+                "苏渝",
+                "65827782610e49f8b9c9ec984b67f955"
+            )
+            .unwrap()
+            .resp_code,
+            "0000"
+        );
+        assert_eq!(
+            super::validate_codename(
+                "510108197205052138",
+                "苏渝",
+                "65827782610e49f8b9c9ec984b67f955"
+            )
+            .unwrap()
+            .resp_code,
+            "0004"
+        );
     }
 }
